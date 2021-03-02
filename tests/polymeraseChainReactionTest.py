7a33c2f3d8845a514f2920491a6a2b59f62250b1
LibreNovus <kimpythonhaskell@gmail.com>
2021-03-02 18:04:00 +0100

    Colors

--- tests/polymeraseChainReactionTest.py
+++ tests/polymeraseChainReactionTest.py
@@ -2,20 +2,21 @@ import sys
 sys.path.insert(0,'..')
 
 from polymeraseChainReaction import Fowardprimer, Reverseprimer
+from color import colors
 
 Fowardprimer("AAGCTGGTTTTGACGAC")
 
 def test_sum():
     print("===Function tests===")
     if Fowardprimer("AAGCTGGTTTTGACGAC") == "5' TTCGACCAAAACTGCTG 3'" and Fowardprimer("CGTTACGTGATGCTAAGCTA") == "5' GCAATGCACTACGATTCGAT 3'":
-        print("1. Fowardprimer Pass")
+        print("1. Fowardprimer" + colors.BLUE + " Passed" + colors.ENDC)
     else:
-        print("1. Fowardprimer failed")
+        print("1. Fowardprimer" + colors.RED + " Failed" + colors.ENDC)
 
     if Reverseprimer("AAGCTGGTTTTGACGAC") == "5' GTCGTCAAAACCAGCTT 3'" and Reverseprimer("CGATTCGATCTCATGACATC") == "5' GATGTCATGAGATCGAATCG 3'":
-        print("2. Reverseprimer Pass")
+        print("2. Reverseprimer" + colors.BLUE + " Passed" + colors.ENDC)
     else:
-        print("2. Reverseprimer failed")
+        print("2. Reverseprimer" + colors.RED + " Failed" + colors.ENDC)
 
 if __name__ == "__main__":
     test_sum()
